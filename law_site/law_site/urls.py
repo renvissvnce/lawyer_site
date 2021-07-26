0f2a9440e82da45f64b2810ba9d59dacbb6df37f
renvissvnce <stefjoost@yandex.ru>
2021-07-26 17:09:41 +0300

    Remaked home page. Adding more lawyer information about himself. Remaked slider. Hi's not working for a while. Now this problem is solved :). Added images for that. Can't find how to give permanent size of pictures. Remaked contacts page. Here added some personal information.
    All commits is html :( Soon will be availible reviews with using DB tables. Now this work in process.

--- law_site/law_site/urls.py
+++ law_site/law_site/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +9,4 @@ urlpatterns = [
     path('', include('prices.urls')),
     path('', include('contacts.urls')),
     path('', include('reviews.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
