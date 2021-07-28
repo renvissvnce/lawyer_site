ea0ca439ce3df8bc32d858d72f05efdf2424b265
renvissvnce <stefjoost@yandex.ru>
2021-07-28 14:34:19 +0300

    make it better

--- law_site/law_site/settings.py
+++ law_site/law_site/settings.py
@@ -1,7 +1,7 @@
 
 from pathlib import Path
 import os
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,4 +124,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
