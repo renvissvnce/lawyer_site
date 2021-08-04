9c6e3f5d380482829f01c57ce1dffa6b2f085b6a
renvissvnce <stefjoost@yandex.ru>
2021-08-04 22:50:33 +0300

    make it better

--- law_site/law_site/settings.py
+++ law_site/law_site/settings.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,7 +76,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+import dj_database_url
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
