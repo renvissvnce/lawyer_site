e1b8be7bda145904822cd4269501493bec272958
renvissvnce <stefjoost@yandex.ru>
2021-08-04 23:44:40 +0300

    make it better

--- law_site/law_site/settings.py
+++ law_site/law_site/settings.py
@@ -1,7 +1,7 @@
 
 from pathlib import Path
 import os
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,7 +76,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-import dj_database_url
+
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
