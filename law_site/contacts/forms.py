23ec1a44c14adfc5cf32f3b967b0792be93ad79b
renvissvnce <stefjoost@yandex.ru>
2021-08-09 11:37:49 +0300

    Feedback form functional now

---
+++ law_site/contacts/forms.py
@@ -0,0 +1,36 @@
+from .models import Feedback
+from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
+
+
+class FeedbackForm(ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ['fio', 'phone', 'mail', 'topic', 'full_text', 'date',]
+
+        widgets = {
+            'fio': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ваше ФИО'
+            }),
+            'phone': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ваш телефон'
+            }),
+            'mail': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ваша электронная почта'
+            }),
+            'topic': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Тема вашего обращения'
+            }),
+            'full_text': Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Опишите вашу проблему'
+            }),
+            'date': DateTimeInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ваше ФИО'
+            })
+
+        }
