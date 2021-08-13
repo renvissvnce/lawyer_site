c7ff3664053c987a753650fa663b88b27e2b84d0
renvissvnce <stefjoost@yandex.ru>
2021-08-13 12:12:34 +0300

    make it better

--- law_site/contacts/models.py
+++ law_site/contacts/models.py
@@ -6,7 +6,7 @@ class Feedback(models.Model):
     phone = models.CharField('Ваш телефон', max_length=25)
     mail = models.CharField('Ваша электронная почта', max_length=30)
     topic = models.CharField('Тема обращения', max_length=80)
-    full_text = models.CharField('Опишите вашу тему обращения', max_length=5000)
+    full_text = models.TextField('Опишите вашу тему обращения', max_length=5000)
     date = models.DateTimeField(default=datetime.now, blank=True)
 
 
