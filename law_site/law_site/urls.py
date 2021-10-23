632baa8c334f365ded42b1e320cac3a7aa22fe45
renvissvnce <stefjoost@yandex.ru>
2021-10-23 23:36:19 +0300

    make it better

--- law_site/law_site/urls.py
+++ law_site/law_site/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -7,9 +7,10 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('', include('about.urls')),
     path('', include('prices.urls')),
     path('', include('contacts.urls')),
-    path('', include('reviews.urls')),
+    #path('', include('reviews.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
