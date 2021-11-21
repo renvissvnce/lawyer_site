1815247f1eb8fc4bca5b558547cfc445d1ad0190
renvissvnce <stefjoost@yandex.ru>
2021-11-21 18:27:38 +0300

    make it better

--- law_site/reviews/models.py
+++ law_site/reviews/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class Acc(AbstractUser):
-    fio = models.CharField(max_length=40)
+    fio = models.CharField(max_length=40, blank=True)
     phone = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     username = models.CharField(max_length=20, unique=True)
