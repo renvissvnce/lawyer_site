bb4b8fbe8fa4118bdadf733f4441031aeda310a8
ydd <stefjoost@yandex.ru>
2022-01-13 13:33:33 +0300

    Working review author

--- law_site/law_site/settings.py
+++ law_site/law_site/settings.py
@@ -133,6 +133,6 @@ AUTH_USER_MODEL = "reviews.Acc"
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'tatestrus@gmail.com'
-EMAIL_HOST_PASSWORD = "asd123rf"
+EMAIL_HOST_PASSWORD = 'asd123rf'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
